46a70306a6242ec1f1081407bc3d09e318216ff7
Shubham Bansal <bansall.shubham@gmail.com>
2024-02-18 22:21:03 +0530

    Simple chat app complete

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -1,33 +1,56 @@
-import { Server } from 'socket.io'
+import { Server } from "socket.io";
+import Redis from "ioredis";
+
+const pub = new Redis({
+  host: "redis-253a7749-redis-bansal.a.aivencloud.com",
+  username: "default",
+  password: "AVNS_9LTwMARLgIleVld0tYF",
+  port: 23745,
+});
+const sub = new Redis({
+  host: "redis-253a7749-redis-bansal.a.aivencloud.com",
+  username: "default",
+  password: "AVNS_9LTwMARLgIleVld0tYF",
+  port: 23745,
+});
 
 class SocketService {
-    private _io: Server;
+  private _io: Server;
 
-    constructor() {
-        console.log("Init Socket ")
-        this._io = new Server({
-            cors: {
-                allowedHeaders: ['*'],
-                origin: "*"
-            }
-        });
-    }
+  constructor() {
+    console.log("Init Socket ");
+    this._io = new Server({
+      cors: {
+        allowedHeaders: ["*"],
+        origin: "*",
+      },
+    });
+    sub.subscribe("MESSAGES");
+  }
 
-    public initListeners() {
-        const io = this.io;
-        console.log("Init Socket Listeners....")
-        io.on("connect", (socket) => {
-            console.log(`New Socket connected`, socket.id)
+  public initListeners() {
+    const io = this.io;
+    console.log("Init Socket Listeners....");
+    io.on("connect", (socket) => {
+      console.log(`New Socket connected`, socket.id);
 
-            socket.on('event:message', async ({ message }: { message: string }) => {
-                console.log("New MEssage Recieved", message);
-            });
-        });
-    }
+      socket.on("event:message", async ({ message }: { message: string }) => {
+        console.log("New MEssage Recieved", message);
+        //publish message to redis
+        await pub.publish("MESSAGES", JSON.stringify({ message }));
+      });
+    });
 
-    get io() {
-        return this._io;
-    }
+    sub.on("message", (channel, message) => {
+      if (channel === "MESSAGES") {
+        io.emit("message", message);
+      }
+    });
+  }
+
+  get io() {
+    return this._io;
+  }
 }
 
 export default SocketService;
